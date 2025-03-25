ec11ff146c02b3a5710c0fd1a930fe7e47845f79
THEROER <theroer09@gmail.com>
2025-03-26 00:56:29 +0200

    Refactor SerializableHttpUrl and SerializableDecimal to implement custom Pydantic core schema methods for enhanced validation and serialization

--- uaproject_backend_schemas/schemas.py
+++ uaproject_backend_schemas/schemas.py
@@ -1,8 +1,10 @@
 from decimal import Decimal
 from enum import StrEnum
 from typing import Any
+from urllib.parse import urlparse
 
-from pydantic import BaseModel, HttpUrl
+from pydantic import BaseModel, GetCoreSchemaHandler
+from pydantic_core import CoreSchema, core_schema
 
 __all__ = [
     "SortOrder",
@@ -36,37 +38,80 @@ class RedirectUrlResponse(BaseModel):
     url: str
 
 
-class SerializableHttpUrl(HttpUrl):
-    def __str__(self):
-        return str(self)
-
+class SerializableHttpUrl(str):
     @classmethod
-    def parse_obj(cls, value: Any) -> "SerializableHttpUrl":
-        if isinstance(value, str):
-            return cls(value)
-        return super().parse_obj(value)
+    def __get_pydantic_core_schema__(
+        cls, source_type: Any, handler: GetCoreSchemaHandler
+    ) -> CoreSchema:
+        def validate(value: Any) -> str:
+            if isinstance(value, str):
+                # Validate URL
+                try:
+                    result = urlparse(value)
+                    if all([result.scheme, result.netloc]):
+                        return value
+                except Exception:
+                    raise ValueError("Invalid HTTP URL")
+            elif isinstance(value, cls):
+                return value
+            raise ValueError("Invalid URL type")
 
-    @classmethod
-    def __get_pydantic_core_schema__(cls, source_type: Any, handler):
-        return {
-            "type": "string",
-            "format": "httpurl",
-        }
+        def serialize(value: Any) -> str:
+            return str(value)
+
+        schema = core_schema.union_schema(
+            [
+                core_schema.is_instance_schema(cls),
+                core_schema.str_schema(),
+            ]
+        )
+
+        return core_schema.no_info_after_validator_function(
+            validate,
+            schema,
+            serialization=core_schema.plain_serializer_function_ser_schema(
+                serialize, when_used="json"
+            ),
+        )
+
+    def __str__(self) -> str:
+        return super().__str__()
 
 
 class SerializableDecimal(Decimal):
-    def __str__(self):
-        return str(self)
-
     @classmethod
-    def parse_obj(cls, value: Any) -> "SerializableDecimal":
-        if isinstance(value, str):
-            return cls(value)
-        return super().parse_obj(value)
+    def __get_pydantic_core_schema__(
+        cls, source_type: Any, handler: GetCoreSchemaHandler
+    ) -> core_schema.CoreSchema:
+        def validate(value: Any) -> Decimal:
+            if isinstance(value, str):
+                try:
+                    return Decimal(value)
+                except Exception:
+                    raise ValueError("Invalid Decimal value")
+            elif isinstance(value, (int, float, Decimal, cls)):
+                return Decimal(value)
+            raise ValueError("Invalid Decimal type")
 
-    @classmethod
-    def __get_pydantic_core_schema__(cls, source_type: Any, handler):
-        return {
-            "type": "string",
-            "format": "decimal",
-        }
+        def serialize(value: Any) -> str:
+            return str(value)
+
+        schema = core_schema.union_schema(
+            [
+                core_schema.is_instance_schema(cls),
+                core_schema.str_schema(),
+                core_schema.float_schema(),
+                core_schema.int_schema(),
+            ]
+        )
+
+        return core_schema.no_info_after_validator_function(
+            validate,
+            schema,
+            serialization=core_schema.plain_serializer_function_ser_schema(
+                serialize, when_used="json"
+            ),
+        )
+
+    def __str__(self) -> str:
+        return super().__str__()
