7c1e6d4555c1e8914a6eb5d0587db9ac95eae98e
THEROER <theroer09@gmail.com>
2025-06-08 10:20:04 +0300

    Changed from Integer to BigInteger in UserRoles

--- uaproject_backend_schemas/models/user_roles.py
+++ uaproject_backend_schemas/models/user_roles.py
@@ -1,19 +1,18 @@
-from typing import TYPE_CHECKING
 
-from sqlmodel import Column, ForeignKey, Integer, Relationship
+from sqlalchemy import BigInteger
+from sqlmodel import Column, ForeignKey
 
 from uaproject_backend_schemas.awesome.fields import AwesomeField
 from uaproject_backend_schemas.awesome.mixins import TimestampsMixin
 from uaproject_backend_schemas.awesome.model import AwesomeModel
 
-if TYPE_CHECKING:
-    from uaproject_backend_schemas.models.role import Role
-    from uaproject_backend_schemas.models.user import User
-
 
 class UserRoles(AwesomeModel, TimestampsMixin, table=True):
     __tablename__ = "user_roles"
 
-    user_id: int = AwesomeField(sa_column=Column(Integer, ForeignKey("users.id"), primary_key=True))
-    role_id: int = AwesomeField(sa_column=Column(Integer, ForeignKey("roles.id"), primary_key=True))
-
+    user_id: int = AwesomeField(
+        sa_column=Column(BigInteger, ForeignKey("users.id"), primary_key=True)
+    )
+    role_id: int = AwesomeField(
+        sa_column=Column(BigInteger, ForeignKey("roles.id"), primary_key=True)
+    )
