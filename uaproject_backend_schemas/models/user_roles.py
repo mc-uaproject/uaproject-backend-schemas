8ace889684c9bb26c28a611a301645f8ec51e714
THEROER <theroer09@gmail.com>
2025-06-21 14:52:16 +0300

    Added the __scope_prefix__ attribute to the UserRoles model to improve code organization and structure.

--- uaproject_backend_schemas/models/user_roles.py
+++ uaproject_backend_schemas/models/user_roles.py
@@ -8,6 +8,7 @@ from uaproject_backend_schemas.awesome.model import AwesomeModel
 
 class UserRoles(AwesomeModel, TimestampsMixin, table=True):
     __tablename__ = "user_roles"
+    __scope_prefix__ = "user_roles"
 
     user_id: int = AwesomeField(
         sa_column=Column(BigInteger, ForeignKey("users.id"), primary_key=True)
